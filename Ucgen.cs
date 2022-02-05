cc2781d5d6fcfe60028199370d2180d78a39d951
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-06 02:41:17 +0300

    Ucgen Sınıfında üçgeni oluşturduğum UcgenCiz Metodunun ilk denemesini yaptım.

--- Ucgen.cs
+++ Ucgen.cs
@@ -1,7 +1,38 @@
+using System;
 namespace Triangle
 {
     public class Ucgen
     {
-        
+        public void UcgenCiz(int UcgenUzunlugu)
+        {
+            for (int i = 1; i <= UcgenUzunlugu; i++)
+            {
+                for (int j = 0; j < UcgenUzunlugu-i; j++)
+                {
+                    Console.WriteLine(" ");
+                }
+
+                for (int k = 0;  k< 2*i-1; k++)
+                {
+                    Console.WriteLine("*");
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+/*
+    *          1
+   ***         3
+  *****        5
+ *******       7
+*********      9
+    11
+
+*/
