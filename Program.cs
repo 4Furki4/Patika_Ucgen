04e67944207d59f7c01b09563c5ea9e8076cae55
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-06 02:11:49 +0300

    Projede kullanılacak sınıflar oluşturuldu.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ucgen.cs
@@ -0,0 +1,7 @@
+namespace Triangle
+{
+    public class Ucgen
+    {
+        
+    }
+}
