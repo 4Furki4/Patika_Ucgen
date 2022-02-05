f94cb7823e2f3ced1d7b528ad790a66489bee879
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-06 02:16:39 +0300

    Konsol_Islemcisine Konsola ilk mesajı yazdıran ve değer isteyen GirdiMesajı adlı metot ile UcgenUzunluguAlma adlı metot eklendi.

--- Konsol_Islemcisi.cs
+++ Konsol_Islemcisi.cs
@@ -1,7 +1,16 @@
+using System;
 namespace Triangle
 {
     public class Konsol_Islemcisi
     {
-        
+        public void GirdiMesajı()
+        {
+            System.Console.Write("Lütfen üçgenin yüksekliğini giriniz:");
+        }
+        public int UcgenUzunluguAlma()
+        {
+            int uzunluk= int.Parse(Console.ReadLine());
+            return uzunluk;
+        }
     }
 }
